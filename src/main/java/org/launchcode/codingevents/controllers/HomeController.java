ccc6683e09d1c1b93ed19e64cd5ce910e46ec4a3
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-01-23 16:11:21 -0600

    test

--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * Created by Chris Bay
+ * Created by Chris Bayfgdfg
  */
 @Controller
 public class HomeController {
